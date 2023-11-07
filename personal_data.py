44a842ff09fc5de76afbff0aade7db0b52d928e6
cooperaveri <143155194+cooperaveri@users.noreply.github.com>
2023-11-06 19:44:14 -0500

    personal_data.py

---
+++ personal_data.py
@@ -0,0 +1,72 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "05aab51d-06eb-4515-b58c-0cc1917672bc",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "class personaldata:\n",
+    "    def __init__(self, name, address, age, phone_number):\n",
+    "        self.name = name\n",
+    "        self.address = address\n",
+    "        self.age = age\n",
+    "        self.phone_number = phone_number\n",
+    "\n",
+    "    def get_name(self):\n",
+    "        return self.name\n",
+    "\n",
+    "    def get_address(self):\n",
+    "        return self.address\n",
+    "\n",
+    "    def get_age(self):\n",
+    "        return self.age\n",
+    "\n",
+    "    def get_phone_number(self):\n",
+    "        return self.phone_number\n",
+    "\n",
+    "    def set_name(self, name):\n",
+    "        self.name = name\n",
+    "\n",
+    "    def set_address(self, address):\n",
+    "        self.address = address\n",
+    "\n",
+    "    def set_age(self, age):\n",
+    "        self.age = age\n",
+    "\n",
+    "    def set_phone_number(self, phone_number):\n",
+    "        self.phone_number = phone_number\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "235ddb09-7b66-4109-b474-d91504c9a4b1",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "anaconda-2022.05-py39",
+   "language": "python",
+   "name": "conda-env-anaconda-2022.05-py39-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
